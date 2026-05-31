3f7c1c3b9381a14537220711e536c32cb6d7a607
tileOtter <adamfoster913@gmail.com>
2026-05-31 17:29:01 -0400

    repl update

--- repl.go
+++ repl.go
@@ -7,9 +7,5 @@ import (
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
 	split := strings.Fields(lowered)
-	trimmed := []string{}
-	for _, s := range split {
-		trimmed = append(trimmed, strings.TrimSpace(s))
-	}
 	return split
 }
