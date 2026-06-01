19eeb6cc9b14b37cc2b9cd536c96a4bab03e5ea5
tileOtter <adamfoster913@gmail.com>
2026-05-31 21:53:55 -0400

    Added startRepl function, updated main to call it.

--- repl.go
+++ repl.go
@@ -1,8 +1,22 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		input := scanner.Text()
+		cleanedStringMap := cleanInput(input)
+		fmt.Printf("Your command was: %s\n", cleanedStringMap[0])
+	}
+}
 
 func cleanInput(text string) []string {
 	lowered := strings.ToLower(text)
