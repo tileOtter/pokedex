93dcd2c61038aae0e3616bcf228148c094c7a13e
tileOtter <adamfoster913@gmail.com>
2026-06-04 04:36:18 -0400

    added error handling for Scanner

--- repl.go
+++ repl.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
@@ -18,6 +19,9 @@ func cleanInput(text string) []string {
 func startRepl(cfg *pokeapi.Config) {
 	cmdMap := getCommandsMap()
 	scanner := bufio.NewScanner(os.Stdin)
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("error reading input: %s", err)
+	}
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
